8c91f47de8dbd7e490cd421eaaec4eeeac308628
Hira-Yuki <cjsdlf44@gmail.com>
2023-06-13 22:52:30 +0900

    로그인 버튼 이미지 추가, 라우터 돔 사용과 api 사용을 위한 파일 추가

---
+++ src/Components/feature/Kakaologin/Kakaologin.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import kakaobtn from "./kakao_login_medium_narrow.png";
+
+function Kakaologin() {
+    const kakaologinApi = ""
+  return;
+  <a href={`${kakaologinApi}`}>
+      <img src={kakaobtn} alt="kakao Login button"/>
+    </a>;
+}
+
+export default Kakaologin;
