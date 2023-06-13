24f49851de891fe963ba769443950c0bf8335812
Hira-Yuki <cjsdlf44@gmail.com>
2023-06-13 23:32:58 +0900

    린트 파일 수정 카카오 로그인 컴포넌트 문법 오류 수정

--- src/Components/feature/Kakaologin/Kakaologin.jsx
+++ src/Components/feature/Kakaologin/Kakaologin.jsx
@@ -3,10 +3,11 @@ import kakaobtn from "./kakao_login_medium_narrow.png";
 
 function Kakaologin() {
   const kakaologinApi = "http://abc.com";
-  return;
-  <a href={kakaologinApi}>
-    <img src={kakaobtn} alt="kakao Login button" />
-  </a>;
+  return (
+    <a href={kakaologinApi}>
+      <img src={kakaobtn} alt="kakao Login button" />
+    </a>
+  );
 }
 
 export default Kakaologin;
