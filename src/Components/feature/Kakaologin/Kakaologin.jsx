fbb752eb7e1c1b9cf2558d60be5b139c1b93ab85
Hira-Yuki <cjsdlf44@gmail.com>
2023-06-13 23:10:42 +0900

    Lint 업데이트. 카카오 버튼 삽입

--- src/Components/feature/Kakaologin/Kakaologin.jsx
+++ src/Components/feature/Kakaologin/Kakaologin.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import kakaobtn from "./kakao_login_medium_narrow.png";
 
 function Kakaologin() {
-    const kakaologinApi = ""
+  const kakaologinApi = "http://abc.com";
   return;
-  <a href={`${kakaologinApi}`}>
-      <img src={kakaobtn} alt="kakao Login button"/>
-    </a>;
+  <a href={kakaologinApi}>
+    <img src={kakaobtn} alt="kakao Login button" />
+  </a>;
 }
 
 export default Kakaologin;
