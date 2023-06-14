7ad33356e8797958115ac0ad4c219cfeb29103bb
Hira-Yuki <cjsdlf44@gmail.com>
2023-06-14 23:36:12 +0900

    에디터 패키지 세팅

---
+++ src/Components/feature/Editor/Editor.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+function Editor() {
+  const [bodyContents, setBodyContent] = useState("");
+  const onChangeHandler = (contents) => {
+    setBodyContent(contents);
+  };
+
+  return (
+    <div>
+      <ReactQuill onChange={onChangeHandler} modules={modules} />
+      <div dangerouslySetInnerHTML={{ __html: bodyContents }} />
+      {/* {process.browser ? (
+        <div dangerouslySetInnerHTML={{ __html: bodyContents }} />
+      ) : (
+        <div />
+      )} */}
+    </div>
+  );
+}
+
+export default Editor;
+
+// 에디터의 모듈 
+const modules = {
+  toolbar: {
+    container: [
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      [{ font: [] }],
+      [{ align: [] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [{ list: "ordered" }, { list: "bullet" }, "link"],
+      [
+        {
+          color: [
+            "#000000",
+            "#e60000",
+            "#ff9900",
+            "#ffff00",
+            "#008a00",
+            "#0066cc",
+            "#9933ff",
+            "#ffffff",
+            "#facccc",
+            "#ffebcc",
+            "#ffffcc",
+            "#cce8cc",
+            "#cce0f5",
+            "#ebd6ff",
+            "#bbbbbb",
+            "#f06666",
+            "#ffc266",
+            "#ffff66",
+            "#66b966",
+            "#66a3e0",
+            "#c285ff",
+            "#888888",
+            "#a10000",
+            "#b26b00",
+            "#b2b200",
+            "#006100",
+            "#0047b2",
+            "#6b24b2",
+            "#444444",
+            "#5c0000",
+            "#663d00",
+            "#666600",
+            "#003700",
+            "#002966",
+            "#3d1466",
+            "custom-color",
+          ],
+        },
+        { background: [] },
+      ],
+    ],
+  },
+};
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "../Pages/Home";
+import Home from "Pages/Home";
+import Editor from "Components/feature/Editor/Editor";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/editor" element={<Editor />} />
         {/* <Route path="/job" element={<PostList />} />
-        <Route path="posting" element={<Posting />} />
+        
         <Route path="/job/:job_id" element={<Detailpage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Signin />} />
